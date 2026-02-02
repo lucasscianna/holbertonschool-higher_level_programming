a0020cd35804b61021e02e4a9d956c091ce9ef4c
lucasscianna <sciannalucas0@gmail.com>
2026-02-02 14:23:16 +0000

    .

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""Module that defines a function to list attributes and methods of an object."""
+def lookup(obj):
+    """Return the list of available attributes and methods of an object."""
+    return dir(obj)
