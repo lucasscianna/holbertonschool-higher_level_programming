049408591d0381de1eb9f9f23e19863079c16f9d
lucasscianna <sciannalucas0@gmail.com>
2026-02-12 13:11:26 +0000

    .

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Load object from JSON file."""
+import json
+
+
+def load_from_json_file(filename):
+    """Load object from JSON file."""
+    with open(filename, 'r', encoding="utf-8") as f:
+        return json.load(f)
