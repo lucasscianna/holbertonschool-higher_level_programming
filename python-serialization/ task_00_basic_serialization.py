b0f0cb6fc771ec5b4ef38455d67b6c9c200bcebc
lucasscianna <sciannalucas0@gmail.com>
2026-02-12 14:52:06 +0000

    .

--- python-serialization/ task_00_basic_serialization.py
+++ python-serialization/ task_00_basic_serialization.py
@@ -1,7 +1,16 @@
+#!/usr/bin/python3
+"""JSON dict serialize/deserialize utilities."""
+import json
+
+
 def serialize_and_save_to_file(data, filename):
-    # Your code here to serialize and save data to the specified file
-    pass
+    """Serialize dict to JSON file."""
+    with open(filename, 'w', encoding="utf-8") as f:
+        json.dump(data, f)
+
 
 def load_and_deserialize(filename):
-    # Your code here to load and deserialize data from the specified file
-    pass
+    """Load JSON file to dict."""
+    with open(filename, 'r', encoding="utf-8") as f:
+        return json.load(f)
+
