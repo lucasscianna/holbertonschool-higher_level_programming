ae3fcf7cc041f528e95c825ca394703f35c94ee3
lucasscianna <you@example.com>
2026-03-04 16:56:52 +0000

    .

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- liste les scores supérieurs ou égaux à 10 dans la table second_table
-SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
