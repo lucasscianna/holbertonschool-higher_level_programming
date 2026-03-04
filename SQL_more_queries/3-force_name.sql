811b9fc16f228c023af49f6ce0e1418ab95e273f
lucasscianna <you@example.com>
2026-03-04 17:30:54 +0000

    .

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,6 @@
+-- Creates the table force_name on the MySQL server
+-- The table has id (INT) and name (VARCHAR(256)) NOT NULL
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
