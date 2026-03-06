bc4a68c1db25027cf44f55f21c90d7c8547f0b27
lucasscianna <you@example.com>
2026-03-06 20:42:08 +0000

    .

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,35 +1,46 @@
 #!/usr/bin/python3
 """
-Lists all states from the database hbtn_0e_0_usa.
-Usage: ./0-select_states.py <mysql username> \
-                           <mysql password> \
-                           <database name>
+This module lists all states from the database hbtn_0e_0_usa.
+The script takes 3 arguments: mysql username, password and database name.
+It uses the MySQLdb module to connect to a MySQL server on localhost.
 """
 import MySQLdb
 import sys
 
 
-if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
+def select_states():
+    """
+    Connects to the database and fetches all states sorted by id.
+    """
+    if len(sys.argv) != 4:
+        return
+
+    u_name = sys.argv[1]
+    u_pass = sys.argv[2]
     db_name = sys.argv[3]
 
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=db_name
-    )
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=u_name,
+            passwd=u_pass,
+            db=db_name
+        )
 
-    cursor = db.cursor()
+        cursor = db.cursor()
+        
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
 
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        cursor.close()
+        db.close()
+    except Exception:
+        pass
 
-    query_rows = cursor.fetchall()
 
-    for row in query_rows:
-        print(row)
-
-    cursor.close()
-    db.close()
+if __name__ == "__main__":
+    select_states()
