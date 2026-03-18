d44f6805ac2405fb498cd708403132a7a73ce244
lucasscianna <you@example.com>
2026-03-18 21:37:25 +0000

    .

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) {
-    console.log(0);
-  } else {
-    const args = process.argv.slice(2).map(Number);
-    const uniqueArgs = [...new Set(args)];
-    uniqueArgs.sort((a, b) => b - a);
-  
-    if (uniqueArgs.length < 2) {
-      console.log(0);
-    } else {
-      console.log(uniqueArgs[1]);
-    }
+let first = 0;
+let second = 0;
+
+for (let i = 2; i < process.argv.length; i++) {
+  if (parseInt(process.argv[i]) > first) {
+    second = first;
+    first = parseInt(process.argv[i]);
+  } else if (parseInt(process.argv[i]) > second) {
+    second = parseInt(process.argv[i]);
   }
+}
+console.log(second);
