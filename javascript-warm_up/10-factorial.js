7a4f483620f792eb59b11ff0aac205f34fce342d
lucasscianna <you@example.com>
2026-03-18 21:36:33 +0000

    .

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 function factorial (n) {
-    if (isNaN(n) || n === 0 || n === 1) {
-      return 1;
-    }
-    return n * factorial(n - 1);
+  if (n === 0 || isNaN(n)) {
+    return 1;
   }
-
-  const arg = parseInt(process.argv[2]);
-  console.log(factorial(arg));
+  return n * factorial(n - 1);
+}
+console.log(factorial(parseInt(process.argv[2])));
